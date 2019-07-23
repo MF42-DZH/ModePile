364e84a104f70d448c26b050120f7ac2f386b9d2
Shots243 <fawwazandsyafiq@gmail.com>
2019-07-23 09:25:23 +0100

    Added one board as a test.

--- src/zeroxfc/nullpo/custom/modes/objects/expressshipping/Boards.java
+++ src/zeroxfc/nullpo/custom/modes/objects/expressshipping/Boards.java
@@ -2,4 +2,24 @@ package zeroxfc.nullpo.custom.modes.objects.expressshipping;
 
 public class Boards {
 	// Static fields here.
+	public static final int[][][] Boards = new int[][][] {
+			new int[][] {
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+					new int[] { 0, 1, 1, 0, 0, 1, 1, 0 },
+					new int[] { 0, 1, 0, 0, 0, 0, 1, 0 },
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+					new int[] { 0, 0, 0, 1, 1, 0, 0, 0 },
+					new int[] { 0, 0, 0, 1, 1, 0, 0, 0 },
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+					new int[] { 0, 1, 0, 0, 0, 0, 1, 0 },
+					new int[] { 0, 1, 1, 0, 0, 1, 1, 0 },
+					new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+			}
+	};
 }
