df69490761a42dda831178adeb4797a801c071fb
Shots243 <fawwazandsyafiq@gmail.com>
2019-07-24 08:23:50 +0100

    Setup mode owner and receivers.

--- src/zeroxfc/nullpo/custom/modes/ExpressShipping.java
+++ src/zeroxfc/nullpo/custom/modes/ExpressShipping.java
@@ -1,12 +1,16 @@
 package zeroxfc.nullpo.custom.modes;
 
+import mu.nu.nullpo.game.event.EventReceiver;
 import mu.nu.nullpo.game.play.GameEngine;
+import mu.nu.nullpo.game.play.GameManager;
 import zeroxfc.nullpo.custom.libs.*;
 import zeroxfc.nullpo.custom.modes.objects.expressshipping.*;
 
 public class ExpressShipping extends PuzzleGameEngine {
 	// This is literally Puzzle Express.
 
+	private GameManager owner;
+	private EventReceiver receiver;
 	private WeightedRandomiser pieceRandomiser;
 	private GamePiece[] conveyorBelt;
 
