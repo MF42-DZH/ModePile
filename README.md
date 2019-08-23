76b8794c54f644e38b627b5d902ff9fc430fca9f
Shots243 <fawwazandsyafiq@gmail.com>
2019-08-23 17:30:43 +0100

    Added library usage terms in USAGE.txt and in all library files.

--- README.md
+++ README.md
@@ -80,5 +80,5 @@ Requires the above to compile and run. The "res" directory in the "required file
 * ry00001 - A main inspiration for starting this modding project, a code design helper and motivator to allow this to continue.
 * GlitchyPSI - A main inspiration for starting this modding project, a code design helper (esp. for allowing the adaptation of their custom sound loader code) and motivator to allow this to continue.
 * Akari - A main inspiration for starting this modding project (see *Idiot% Mode* above) and motivator to allow this to continue.
-* NullNoName - Creator of *NullpoMino*.
+* The original creator of *NullpoMino*.
 * All others that worked on *NullpoMino* - For helping to develop the game into the extensible platform we know today.
--- src/zeroxfc/nullpo/custom/libs/Interpolation.java
+++ src/zeroxfc/nullpo/custom/libs/Interpolation.java
@@ -1,3 +1,36 @@
+/*
+ * This library class was created by 0xFC963F18DC21 / Shots243
+ * It is part of an extension library for the game NullpoMino (copyright 2010)
+ *
+ * Herewith shall the term "Library Creator" be given to 0xFC963F18DC21.
+ * Herewith shall the term "Game Creator" be given to the original creator of NullpoMino.
+ *
+ * THIS LIBRARY AND MODE PACK WAS NOT MADE IN ASSOCIATION WITH THE GAME CREATOR.
+ *
+ * Repository: https://github.com/Shots243/ModePile
+ *
+ * When using this library in a mode / library pack of your own, the following
+ * conditions must be satisfied:
+ *     - This license must remain visible at the top of the document, unmodified.
+ *     - Only binaries of this library are allowed to be distributed unless at the
+ *       permission of the Library Creator.
+ *         - If this is the case, the Library Creator must be credited somewhere.
+ *             - Source comments only are fine, but in a README is recommended.
+ *     - Modification of this library is allowed, but only in the condition that a
+ *       pull request is made to merge the changes to the repository.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package zeroxfc.nullpo.custom.libs;
 
 public class Interpolation {
