9a4437cd87bd76c8b90bee18872fc354e8cbdd04
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-03-29 20:41:59 +0100

    Use my custom image instead.

--- ci-build-task.sh
+++ ci-build-task.sh
@@ -3,4 +3,6 @@
 curl -L -O https://github.com/nullpomino/nullpomino/archive/refs/tags/v7.5.0.zip
 unzip ./nullpomino-v7.5.0.zip
 mv ./nullpomino-v7.5.0/* .
-/usr/bin/env sh ./build.sh
+mkdir -p ./bin
+find -wholename "./src/*.java" > .TMPSRC
+javac -source 8 -target 8 -encoding utf8 -sourcepath ./src -d ./bin -cp ./lib/* @.TMPSRC
