23f39dced5304a7b9ef9da61e6a8a5f2a5e2f89f
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-03-29 21:08:19 +0100

    Use CP instead of MOVE to allow merging???

--- ci-build-task.sh
+++ ci-build-task.sh
@@ -2,7 +2,7 @@
 
 curl -L -O https://github.com/nullpomino/nullpomino/archive/refs/tags/v7.5.0.tar.gz
 tar -xf ./v7.5.0.tar.gz
-mv ./nullpomino-7.5.0/* .
+cp -a ./nullpomino-7.5.0/* .
 mkdir -p ./bin
 find -wholename "./src/*.java" > .TMPSRC
 javac -source 8 -target 8 -encoding utf8 -sourcepath ./src -d ./bin -cp ./lib/* @.TMPSRC
