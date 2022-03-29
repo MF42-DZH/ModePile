012f0ef008207f7cad94f2f37442bfc5b010bc3b
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-03-29 20:58:56 +0100

    Thank you cURL, very cool.

--- ci-build-task.sh
+++ ci-build-task.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 curl -L -O https://github.com/nullpomino/nullpomino/archive/refs/tags/v7.5.0.tar.gz
-tar -xf ./nullpomino-7.5.0.tar.gz
+tar -xf ./v7.5.0.tar.gz
 mv ./nullpomino-7.5.0/* .
 mkdir -p ./bin
 find -wholename "./src/*.java" > .TMPSRC
