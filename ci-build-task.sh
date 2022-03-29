1b9fcb333070294afc543c68d331868aec2eee02
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-03-29 20:44:03 +0100

    Use tar instead of unzip.

--- ci-build-task.sh
+++ ci-build-task.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 curl -L -O https://github.com/nullpomino/nullpomino/archive/refs/tags/v7.5.0.zip
-unzip ./nullpomino-v7.5.0.zip
+tar -xf ./nullpomino-v7.5.0.zip
 mv ./nullpomino-v7.5.0/* .
 mkdir -p ./bin
 find -wholename "./src/*.java" > .TMPSRC
