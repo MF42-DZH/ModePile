5f5e1e62f63ca817dfe6401db512c86ca57b7b63
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2022-03-29 20:46:47 +0100

    Fixed misnames.

--- ci-build-task.sh
+++ ci-build-task.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-curl -L -O https://github.com/nullpomino/nullpomino/archive/refs/tags/v7.5.0.zip
-tar -xf ./nullpomino-v7.5.0.zip
-mv ./nullpomino-v7.5.0/* .
+curl -L -O https://github.com/nullpomino/nullpomino/archive/refs/tags/v7.5.0.tar.gz
+tar -xf ./nullpomino-7.5.0.tar.gz
+mv ./nullpomino-7.5.0/* .
 mkdir -p ./bin
 find -wholename "./src/*.java" > .TMPSRC
 javac -source 8 -target 8 -encoding utf8 -sourcepath ./src -d ./bin -cp ./lib/* @.TMPSRC
