00b5fe17f0a099f02b79041e647fba05e3196d60
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2025-06-30 13:01:22 +0100

    Move package `bin.sylveon` -> `network.nightshade`

--- src/network/nightsahde/nullpo/custom/mods/SubscriberChallenge.java
+++ src/network/nightsahde/nullpo/custom/mods/SubscriberChallenge.java
@@ -26,7 +26,7 @@
     ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
     POSSIBILITY OF SUCH DAMAGE.
 */
-package bin.sylveon.nullpomino.mods;
+package network.nightsahde.nullpo.custom.mods;
 
 import mu.nu.nullpo.game.component.BGMStatus;
 import mu.nu.nullpo.game.component.Block;
