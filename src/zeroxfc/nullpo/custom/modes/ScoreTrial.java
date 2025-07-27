8848339a356f864d521154ef8ebde434f25e52bb
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2025-07-27 22:20:50 +0100

    Adjust interpolator parameters for ScoreTrial

--- src/zeroxfc/nullpo/custom/modes/ScoreTrial.java
+++ src/zeroxfc/nullpo/custom/modes/ScoreTrial.java
@@ -118,7 +118,7 @@ public class ScoreTrial extends MarathonModeBase {
     private LandingParticles landingParticles;
     private boolean hardDropEffect;
 
-    private static final Interpolation.GGCE SCORE_INTERPOLATOR = new Interpolation.GGCE(1.06, 0.9 * 0.9, true);
+    private static final Interpolation.GGCE SCORE_INTERPOLATOR = new Interpolation.GGCE(1.06, 1d / 6d, true);
 
     // Mode name
     public String getName() {
